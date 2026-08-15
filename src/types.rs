8893b75bc06e10a3349bd1eb5ae6a96da19f758c
AalbatrossGuy <thexcelsiorisback@gmail.com>
2026-08-15 10:15:50 +0530

    Add db error types

--- src/types.rs
+++ src/types.rs
@@ -36,3 +36,56 @@ impl fmt::Display for Key {
         }
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct Value(pub Vec<u8>);
+
+impl Value {
+    pub fn new(data: impl Into<Vec<u8>>) -> Self {
+        Self(data.into())
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+
+    pub fn length(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl From<&str> for Value {
+    fn from(string: &str) -> Self {
+        Self(string.as_bytes().to_vec())
+    }
+}
+
+impl From<String> for Value {
+    fn from (string: String) -> Self {
+        Self(string.into_bytes())
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DBRecordType {
+    Put,
+    Delete,
+}
+
+#[derive(Error, Debug)]
+pub enum SlimeDBError {
+    #[error("[SlimeDB] I/O Error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("[SlimeDB] Data Corruption Detected: {0}")]
+    Corruption(String),
+
+    #[error("[SlimeDB] Key Not Found: {0}")]
+    KeyNotFound(String),
+}
+
+pub type Result<T> = std::result::Result<T, SlimeDBError>;
